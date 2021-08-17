47657711cefb86ace775c7df279233a24d8bde64
Simon <semastar00@mail.ru>
2021-08-17 16:24:35 +0700

    bot template

---
+++ home_library_bot/data/config.py
@@ -0,0 +1,12 @@
+from environs import Env
+
+# Теперь используем вместо библиотеки python-dotenv библиотеку environs
+env = Env()
+env.read_env()
+
+BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
+ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
+PGUSER = env.str("PGUSER") # DB's username
+PGPASSWORD = env.str("PGPASSWORD") # DB's password
+DATABASE = env.str("DATABASE") # DB's name
+IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
